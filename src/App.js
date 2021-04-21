dd4f5331110e8a8f894926c7dd3a19578beefcbc
Maddison Bruckelmyer <maddisonbruckelmyer@maddison.hsd1.mn.comcast.net>
2021-04-21 17:53:17 -0500

    simple styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
 import Header from './components/Header';
 import Summary from './components/Summary';
 import Email from './components/Email';
-import GitHub from './components/GitHub';
 
 function App() {
   return (
-    <div className="container">
+    <div>
       <Header />
       <Summary /> 
       <Email />
